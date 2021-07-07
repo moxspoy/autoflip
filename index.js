764770411546d5ad43186f27eb4b851cd00ede27
M Nurilman Baehaqi <mnurilmanbaehaqi@gmail.com>
2021-07-07 10:05:39 +0700

    main file

---
+++ index.js
@@ -0,0 +1,18 @@
+/*
+* GOALS: one click, auto build and delivery
+* WORKFLOW
+* build according to variant
+* deliver into slack/firebase
+*/
+import {getAndroidArtifact} from "./src/utils/file-utils.js";
+import * as SlackService from "./src/services/slack.js";
+import * as CLIUtils from "./src/utils/cli-utils.js";
+
+(async() => {
+    CLIUtils.openingMessage();
+    const files = getAndroidArtifact();
+    if (files && files.length) {
+        await SlackService.sendMessageAndUpload(files);
+        CLIUtils.showSuccessMessage();
+    }
+})()
