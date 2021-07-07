f8b0ce1de5e5f3795c1c7f910941949075637446
M Nurilman Baehaqi <mnurilmanbaehaqi@gmail.com>
2021-07-07 10:03:53 +0700

    firebase service

---
+++ src/services/firebase.js
@@ -0,0 +1,15 @@
+import {execSync} from 'child_process'
+import dotenv from "dotenv";
+import path from "path";
+
+dotenv.config();
+
+export const upload = (file, isStaging) => {
+    const token = process.env.FIREBASE_TOKEN;
+    const appId = isStaging
+        ? process.env.FIREBASE_APP_ID_IOS_STAGING_RELEASE
+        : process.env.FIREBASE_APP_ID_IOS_PRODUCTION_RELEASE;
+    const releaseNotesFile = path.join(process.cwd(), 'release-notes.txt');
+    const groups = "B2B QA, Flip QA";
+    execSync(`firebase --token ${token} appdistribution:distribute --app ${appId}  --release-notes-file ${releaseNotesFile} --groups ${groups}`)
+}
