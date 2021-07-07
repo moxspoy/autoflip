d45654dc3a7e31f8ecb469b7b37f268dbe903e73
M Nurilman Baehaqi <mnurilmanbaehaqi@gmail.com>
2021-07-07 10:04:30 +0700

    cli utils

---
+++ src/utils/cli-utils.js
@@ -0,0 +1,23 @@
+export const showSuccessMessage = () => {
+    console.log(`
+Alhamdulillah gans, prosesnya sukses dari mulai build sampai upload :))
+    `);
+}
+
+export const openingMessage = () => {
+    console.log(`
+    
+     ___      __    __  .___________.  ______    _______  __       __  .______   
+    /   \\    |  |  |  | |           | /  __  \\  |   ____||  |     |  | |   _  \\  
+   /  ^  \\   |  |  |  | \`---|  |----\`|  |  |  | |  |__   |  |     |  | |  |_)  | 
+  /  /_\\  \\  |  |  |  |     |  |     |  |  |  | |   __|  |  |     |  | |   ___/  
+ /  _____  \\ |  \`--'  |     |  |     |  \`--'  | |  |     |  \`----.|  | |  |      
+/__/     \\__\\ \\______/      |__|      \\______/  |__|     |_______||__| | _|      
+                                                                                 
+
+Start executing...          
+                                                                                 
+    `);
+}
+
+
