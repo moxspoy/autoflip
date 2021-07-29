9fa84bd9d6206582e2dbc56415b8b70fb8cc9546
M Nurilman Baehaqi <mnurilmanbaehaqi@gmail.com>
2021-07-29 11:49:19 +0700

    add slack user

---
+++ src/constants/index.js
@@ -0,0 +1,15 @@
+export const SLACK_USER = {
+    EQI: '<@UP4JV6K9S>',
+    ED: '<@>',
+    ADIT: '<@U01GKP0BREZ>',
+    FADEL : '<@UNNKADQR0>',
+    IZZUDDIIN: '<@U3YNTB8J3>',
+    DAFIQ: '<@UNE9FT9UK>',
+    RAHMI: '<@U021GLY2F4Y>',
+    ZEGO: '<@UMUKQ3WP8>',
+    SOURABH: '<@U01RJAVPBKL>',
+    QA_CONSUMER_APP: '@qa-cons-app',
+    QA_INTERNATIONAL: '@qa-international-transfer',
+    QA_CONSUMER_SOLUTION_GROUP: '@qa-consumer-solutions-group',
+    QA_DOMESTIC: '@qa-domestic-transfer',
+}
