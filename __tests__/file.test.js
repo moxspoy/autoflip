11da2e81b9f258a9658d0ec325bce5d4065d3d89
M Nurilman Baehaqi <mnurilmanbaehaqi@gmail.com>
2021-08-02 17:46:38 +0700

    change getTaskIdFromUrl parent object

--- __tests__/file.test.js
+++ __tests__/file.test.js
@@ -1,9 +1,10 @@
+import * as ClickupService from '../src/services/clickup.js';
 import * as MessageUtil from '../src/utils/message-utils.js';
 
 const validClickUpUrl = 'https://app.clickup.com/t/9nzg9w';
 
 test('ensure the task id from url', () => {
-    const id = MessageUtil.getTaskIdFromUrl(validClickUpUrl);
+    const id = ClickupService.getTaskIdFromUrl(validClickUpUrl);
     const expectedTaskId = '9nzg9w';
     expect(id).toBe(expectedTaskId);
 });
