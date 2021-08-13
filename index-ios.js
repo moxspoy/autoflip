5ea3916ee7714ecf24c139c29dd83ffe5ae9d44d
M Nurilman Baehaqi <mnurilmanbaehaqi@gmail.com>
2021-08-13 08:07:27 +0700

    fix missing js file

--- index-ios.js
+++ index-ios.js
@@ -9,7 +9,7 @@ import * as SlackService from "./src/services/slack.js";
 import * as FirebaseService from "./src/services/firebase.js";
 import * as CLIUtils from "./src/utils/cli-utils.js";
 import minimist from 'minimist';
-import * as ClickupService from "./src/services/clickup";
+import * as ClickupService from "./src/services/clickup.js";
 
 const args = minimist(process.argv.slice(2));
 
